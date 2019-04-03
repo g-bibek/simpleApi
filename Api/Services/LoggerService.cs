55464066c412a326b03cf92f4b1876b9d9872684
bibek5770 <Bibekg@eriskcorp.com>
2019-04-03 11:14:02 -0500

    initial api setup
    setup api configurations and added an endpoint to fetch current date time

---
+++ Api/Services/LoggerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace Api.Services
+{
+    interface ILoggerService
+    {
+        void LogCurrentRequest(HttpContext context, Exception exception);
+    }
+    public class LoggerService : ILoggerService
+    {
+        public void LogCurrentRequest(HttpContext context, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Api/Startup.cs
@@ -0,0 +1,69 @@
+﻿using Api.AppConfiguration;
+using Api.Middlewares;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Api
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.ConfigureMvcAndJson(Configuration);
+            services.ConfigureDependencyInjection(Configuration);
+
+            services.AddSwaggerGen(c =>
+            {
+                //c.OperationFilter<AddRequiredHeaderParameter>();
+                c.SwaggerDoc("v1", new Info { Title = "Simple Api", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+
+            if (env.IsDevelopment())
+            {
+                loggerFactory.AddConsole();
+                loggerFactory.AddDebug();
+            }
+
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Simple Api");
+            });
+
+            //app.UseMiddleware(typeof(ErrorHandlingMiddleWare));
+
+            app.UseMiddleware(typeof(AuthMiddleware));
+
+            app.UseStaticFiles();
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
